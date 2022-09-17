73abbafdf1aa0a2235649a44fda0f3cab384a76c
Danilo Andrade <danilo_as@live.com>
2022-09-17 07:58:33 -0300

    commit 16/09/2022

--- main.py
+++ main.py
@@ -2,6 +2,23 @@ import streamlit as st
 import subprocess
 import pandas as pd
 
+
+
+st.subheader('1. Printing text in R')
+with st.expander('See code'):
+  code1 = '''print("Hello world ...")
+  '''
+  st.code(code1, language='R')
+process1 = subprocess.Popen(["Rscript", "helloworld.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+result1 = process1.communicate()
+st.write(result1)
+
+
+
+
+
+
+'''
 st.sidebar.markdown('')
 st.sidebar.markdown('')
 st.sidebar.markdown('**Coordinator**: [Danilo Andrade](https://daniloas.com/)')
@@ -12,16 +29,6 @@ codigo = st.text_input('Qual a letra do Código CID-10?')
 
 dataframefinal = pd.DataFrame()
 lista_totais = list()
-
-st.subheader('1. Printing text in R')
-with st.expander('See code'):
-  code1 = '''print("Hello world ...")
-  '''
-  st.code(code1, language='R')
-process1 = subprocess.Popen(["Rscript", "hello.R"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-result1 = process1.communicate()
-st.write(result1)
-
 
 def dados():
     file = str(estado) + '.csv'
@@ -100,3 +107,4 @@ st.download_button(
     "text/csv",
     key='download-csv'
 )
+'''
